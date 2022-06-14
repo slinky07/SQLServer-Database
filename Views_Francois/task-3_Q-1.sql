c409751e9556b0b35dafb0eee11c270ffb7b5639
FBH514 <103460138+FBH514@users.noreply.github.com>
2022-06-13 21:12:05 -0400

    Add files via upload

---
+++ Views_Francois/task-3_Q-1.sql
@@ -0,0 +1,27 @@
+use AragonMunicipalLibrary;
+go
+
+/*
+ Create a mailing list of Library members that includes the members’ full names and complete address information.
+ */
+
+create view MailingList_View as
+select concat_ws(' ', first_name, last_name) as 'Full Name',
+       ad.address_ID, city_ID, unit_number, civic_number, street, province, postal_code as 'Mailing Address'
+
+from Members.Members as m,
+     Members.Adults as a,
+     Members.Addresses as ad
+
+where a.member_id = m.member_id
+  and a.address_id = ad.address_id
+
+
+/*
+ Returns the mailing list of all Library members.
+ */
+
+select *
+from MailingList_View as ml
+;
+go
