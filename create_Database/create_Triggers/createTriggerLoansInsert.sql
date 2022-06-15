4d25f7b34cbd6da59bb4510d92ce31f73bba1a46
francis <f.slinky07@gmail.com>
2022-06-15 15:31:57 -0400

    my triggers

--- create_Database/create_Triggers/createTriggerLoansInsert.sql
+++ create_Database/create_Triggers/createTriggerLoansInsert.sql
@@ -1,4 +1,4 @@
-use AragonMunicipalLibrary;
+-- use AragonMunicipalLibrary;
 
 -- create a trigger on new insertion of a loan to update the on_loan in table Copies tp add 1
 CREATE TRIGGER [Borrows].onLoanInsert
