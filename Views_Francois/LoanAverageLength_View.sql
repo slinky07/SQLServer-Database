200231b967e79f811682863e6443bc6375c0398b
FBH514 <kittens-paring0f@icloud.com>
2022-06-22 16:19:42 -0400

    View to be reviewed

--- Views_Francois/LoanAverageLength_View.sql
+++ Views_Francois/LoanAverageLength_View.sql
@@ -7,9 +7,9 @@
 
 -- make a view calculating the average length of a loan from Borrow.Loans
 create view LoanAverageLength_View as
-select dateadd(day, datediff(day, BL.checkout_date, BL.due_date), 0) as 'Length'
-from Borrows.Loans as BL
+SELECT AVG((DATEDIFF(BL.due_date, BL.checkout_date))) as 'Average length of a loan (in days)'
+From Borrows.Loans as BL;
 
-where BL.checkout_date is not null
 ;
 go
+
--- create_Database/create_Tables/15-Loans.sql
+++ create_Database/create_Tables/15-Loans.sql
@@ -8,6 +8,7 @@ create table Borrows.Loans
     isbn_ID       char(13)      not null, -- check if Items.isbn_ID.copies.loanable == 1 (true)
     member_ID     int      not null, -- check if Members.member_ID.dateadd(year, 1, date_issued) < getdate()
     checkout_date datetime not null,
+    return_date  datetime not null,
     due_date      datetime not null,
     return_status bit      not null
 )
