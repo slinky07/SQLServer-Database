d770cf676e24cf326f2c18445b7e178ef3162447
hershybm@gmail.com <%7S&Kt5HV3k*6*bnxCBK>
2022-06-21 20:46:42 -0400

    - onShelfView gets all copyWideView records where loanable = 'N'

---
+++ Views_Hershey/task-3-Q-8.sql
@@ -0,0 +1,25 @@
+/* Create a view and save it as OnshelfView that queries CopywideView (3-table join). Lists complete
+information about each copy that is not currently on loan (on_loan ='N'). */
+
+use AragonMunicipalLibrary
+;
+go
+
+
+create view ItemCollection.OnShelfView as
+select c.isbn_ID,
+       c.Title,
+       c.publisher,
+       c.binding,
+       c.translation,
+       c.synopsis,
+       c.on_loan,
+       c.copy_amt
+
+from ItemCollection.CopyWideView as c
+
+WHERE c.loanable = 'N'
+;
+go
+
+SELECT * FROM ItemCollection.OnShelfView;
