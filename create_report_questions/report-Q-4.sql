f1f50216bfde1318a444468c0c1055f3ebd30c47
hershybm@gmail.com <Cq4z8KM908fs4V^kblGq>
2022-06-22 18:31:19 -0400

    q4

--- create_report_questions/report-Q-4.sql
+++ create_report_questions/report-Q-4.sql
@@ -8,10 +8,18 @@ create function percentLoanedLastYear()
     returns int
     as
     begin
-        declare @ret int;
-        select @ret = count(distinct isbn_ID)
+        declare @yearCount int;
+        declare @totalCount int;
+
+        set @yearCount = (select count(distinct isbn_ID)
         from Borrows.Loans as l
         where
-            l.checkout_date between DateAdd(yy, -1, GetDate()) and getdate()
-        return @ret;
+            l.checkout_date between DateAdd(yy, -1, GetDate()) and getdate())
+
+        set @totalCount = (select count(distinct isbn_ID) from Borrows.Loans as l)
+
+        if @totalCount = 0
+            select 0;
+        else
+            select @yearCount / @totalCount;
     end
