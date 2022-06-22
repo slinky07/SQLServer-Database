1f6bba39c3fdd3cca6b0a9e01b92d93769361efd
francis <f.slinky07@gmail.com>
2022-06-22 00:54:54 -0400

    changed the main database creation command to drop any existing database of same name before trying to create it.

--- create_Database/1-createDatabase.sql
+++ create_Database/1-createDatabase.sql
@@ -4,13 +4,36 @@ use master
 /**
   main database creation
  */
---
+
+
+ -- -------------------------------------------------------
+-- check if database exists. if it existsm drop then create it, else create it
+
+if exists (select * from sys.databases where name = 'AragonMunicipalLibrary')
+begin
+  print '=============================Database exists. Dropping and re-creating it.============================='
+  drop database AragonMunicipalLibrary;
+  create database AragonMunicipalLibrary;
+end
+else
+begin
+  create database AragonMunicipalLibrary
+end
+;
+go
+use AragonMunicipalLibrary
+
+
+
+---------------------------------------------------------------------------------
+/*
 create database AragonMunicipalLibrary;
 ;
 go
 
 use AragonMunicipalLibrary;
 go
+*/
 
 -- drop database AragonMunicipalLibrary;
 
@@ -25,19 +48,3 @@ go
 
 -- drop database if exists AragonMunicipalLibrary;
 
--- -------------------------------------------------------
--- check if database exists else create it
-
-
-
--- if exists (select * from sys.databases where name = 'AragonMunicipalLibrary')
--- begin
---   print 'Database exists'
---     drop database AragonMunicipalLibrary;
--- end
--- else
--- begin
---   create database AragonMunicipalLibrary
--- end
---
--- use AragonMunicipalLibrary
