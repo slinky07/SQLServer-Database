0e0bb62710d5016fa065e8925c9a89e50911e769
FBH514 <kittens-paring0f@icloud.com>
2022-06-22 16:59:49 -0400

    View to be reviewed

--- Views_Francois/LoanOverduePercentage_View.sql
+++ Views_Francois/LoanOverduePercentage_View.sql
@@ -8,8 +8,15 @@
 -- TODO: this is a view
 
 create view PercentageOverdue as
-    select count(l.loan_ID) as total_loans,
-           (select count(l.loan_ID)from Borrows.Loans as l where l.due_date > l.return_date) as overdue_loans
-    concat('Percentage of all loans that eventually becomes overdue: ', (select count(l.loan_ID) from Borrows.Loans as l where l.due_date > l.return_date) / (select count(l.loan_ID) from Borrows.Loans as l) * 100, '%')
-    ;
-    go
+select *
+from (select count(l.loan_ID) as total_loans,
+             l.return_date    as return_date,
+             l.checkout_date  as checkout_date,
+             l.due_date       as due_date
+
+      from Borrows.Loans as l
+      where (l.due_date > l.return_date) )as overdue_loans
+concat('Percentage of all loans that eventually becomes overdue: ',
+(select count(l.loan_ID) from Borrows.Loans as l where l.due_date > l.return_date) / (select count(l.loan_ID) from Borrows.Loans as l) * 100, '%')
+;
+go
