45a59280dcbb6b6ebe88ec2b82aaba3d2249441b
hershybm@gmail.com <Cq4z8KM908fs4V^kblGq>
2022-06-22 17:18:09 -0400

    Hershey

--- create_report_questions/report-Q-3.sql
+++ create_report_questions/report-Q-3.sql
@@ -1,12 +1,21 @@
 use AragonMunicipalLibrary;
 go
 
-select MAX(total_loans) as Max_Borrowings
-  from (
-      SELECT member_ID
-      , COUNT(loan_ID) as total_loans
-      FROM Borrows.Loans
-      group
-          by member_ID
-           ) as t
-   RETURN
+-- select MAX(total_loans) as Max_Borrowings
+--   from (
+--       SELECT member_ID
+--       , COUNT(loan_ID) as total_loans
+--       FROM Borrows.Loans
+--       group
+--           by member_ID
+--            ) as t
+
+SELECT member_id, COUNT(member_id)
+FROM Borrows.Loans  GROUP BY member_id
+HAVING COUNT (member_id)=(
+SELECT MAX(mycount)
+FROM (
+SELECT member_id, COUNT(member_id) mycount
+FROM Borrows.Loans
+GROUP BY member_id) as mim);
+
