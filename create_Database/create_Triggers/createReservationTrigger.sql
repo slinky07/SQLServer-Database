2ff12a8f6d4c259a577583955dcea18cb377f2dd
francis <f.slinky07@gmail.com>
2022-06-22 21:27:41 -0400

    trigger for reservation table

--- create_Database/create_Triggers/createReservationTrigger.sql
+++ create_Database/create_Triggers/createReservationTrigger.sql
@@ -3,16 +3,17 @@ use AragonMunicipalLibrary
 
 -- reserve trigger when insert , update on)reserve amt is updated +1
 CREATE TRIGGER [Borrows].update_reserve_amt_after_Reservation_change
-    ON Borrows.Loans
+    ON Borrows.Reservations
     AFTER UPDATE, INSERT, DELETE
 AS
     BEGIN
-        declare @calculated_loan int
-        set @calculated_loan = (select ItemCollection.calculateLoanable())
         UPDATE c
-        SET c.loanable = 'Y'
-            from ItemCollection.Copies c
-        WHERE @calculated_loan > 0
+        SET c.on_reserve = c.on_reserve + 1
+            from ItemCollection.Copies c,
+                Borrows.Reservations r,
+                ItemCollection.Items i
+            where r.isbn_ID = i.isbn_ID
+            AND i.copy_ID = c.copy_ID
     END
 ;
 go
