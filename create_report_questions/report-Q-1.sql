92dc3813f067999d1ee46d87a56aaf43d1745435
Nicolay Bang <nicolaybang@gmail.com>
2022-06-22 21:32:49 -0400

    Committing for backup. Almost done.

--- create_report_questions/report-Q-1.sql
+++ create_report_questions/report-Q-1.sql
@@ -3,22 +3,26 @@
 -- Q-2 as well. Dont run this yet as they dont all work.
 
 create view loans_last_year as
-select count(*) from Borrows.Loans as 'Book_Burrowed_Last_Year'
+select count(*) from Borrows.Loans
                 where year(checkout_date) = year(getdate()) - 1;
 /*What percentage of the membership borrowed at least one book?*/
-
-
-
         ;
+/*
+ This view gives a percentage of the membership who borrowed at least one book as well as
+ the total amound of valid members (it calculates based on members who's membership is valid.
+ */
+
+
+
 create function get_loan_count() returns int as
 begin
     declare @loan_count int;
-    select @loan_count = count(*) from Borrows.Loans;
+    set @loan_count = (select count(*) from Borrows.Loans);
     return @loan_count;
 end;
 drop function get_loan_count;
 /*Finds the number of members with valid memberships */
---TODO: We need to have all the members that populate have a card_issue_
+--TODO: We need to have all the members that populate have a valid card_issue_date
   create function get_valid_member_count()
     returns int as
     begin
@@ -27,17 +31,30 @@ drop function get_loan_count;
       return @count;
     end;
 --drop function get_member_count;
-
-select*from Membership.Members;
+--Tested function works
+-- select*from Membership.Members;
 create FUNCTION total_members_borrowed_books()
 RETURNS INTEGER
 BEGIN
 DECLARE @total_members_borrowed_books INTEGER;
-SET @total_members_borrowed_books = (SELECT distinct count(*) FROM Borrows.Loans where Membership.Members.Member_ID = Borrows.Loans.Member_ID)
+SET @total_members_borrowed_books = (SELECT distinct count(*)
+                                     FROM Borrows.Loans, Membership.Members where Borrows.Loans.member_ID = Membership.Members.member_ID);
 RETURN @total_members_borrowed_books;
 
 
 end
+
+create view percentage_borrowed_books as
+select get_loan_count() as total_loan_count,
+         total_members_borrowed_books() as total_membership_count,
+         get_valid_member_count() as valid_membership_count,
+         get_loan_count() / total_members_borrowed_books() as percentage_borrowed_books
+   ;
+-- drop function total_members_borrowed_books
+-- drop function Membership.total_members_borrowed_books;
+-- select * from total_members_borrowed_books();
+-- select * from Borrows.Loans;
+-- select 'Total' from Membership.Members;
 /*Count how many loans did the library do last year?*/
 
 /*
